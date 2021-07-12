c163d0987cfe3ba3c066276b2f7213a52dedcbe3
LaurieBeebee <lbeebee15@heathfieldcc.co.uk>
2021-07-12 13:15:05 +0100

    commit

--- Project_test1.py
+++ Project_test1.py
@@ -4,11 +4,11 @@ import csv
 
 
 class Network:
-    def __init__(self, no_layers, no_neurons, no_initial_inputs, input_activations):
+    def __init__(self, no_layers, no_neurons, input_activations, no_outputs):
         self.layers = int(no_layers)
         self.neurons = int(no_neurons)
-        self.no_ini_inputs = int(no_initial_inputs)
         self.inp_act = input_activations
+        self.no_outputs = int(no_outputs)
 
     #inp_act needs to be inputted as a list of all of the input activations. this can be assigned from the first input or just the input
     #into that layer later on when actually being coded.
@@ -17,20 +17,21 @@ class Network:
     #biases as it is changing and may not want same number of neurons in each layer. also need to think about final layer and how that
     #will work
 
-    def weight_set(self, inp_act):
+    def weight_set(self, inp_act, no_neurons):
         input_weights = []
         weight = []
-        for x in range(0, self.neurons - 1):
-            for i in range(0, len(inp_act)-1):
-                input_weights[i] = random.randint(-1,1)
-            weight[x] = input_weights
+        for x in range(0, no_neurons):
+            for i in range(0, len(inp_act)):
+                rand = random.uniform(-1,1)
+                input_weights.append(rand)
+            weight.append(input_weights)
             input_weights = []
         return weight
 
     def bias_set(self):
         bias = []
-        for i in range(0,self.neurons - 1):
-            x = random.randint(-1,1)
+        for i in range(0,self.neurons):
+            x = random.uniform(-1,1)
             bias.append(int(x))
         return bias
 
@@ -53,16 +54,33 @@ class Network:
             act[i] = self.sigmoid(z[i])
         return act
 
+    def total_weights(self, inp_act):
+        weights = {}
+        input_weights = inp_act
+        for i in range(0, self.layers):
+            weights[i] = self.weight_set(input_weights, self.neurons)
+            input_weights = weights[i]
+            if i+1 == self.layers:
+                weights[i+1] = self.weight_set(input_weights, self.no_outputs)
+        return weights
 
-def main():
-    Iris = open("Iris.csv","r")
-    Irisreader = csv.reader(Iris)
-    test_data_no = random.randint(1,150)
-    for row in Irisreader:
-        if row == test_data_no:
-            test_data = row
-    flower = Network(2,5,6,test_data)
-    
+
+
+
+
+Iris = open("Iris.csv","r")
+Irisreader = csv.reader(Iris)
+test_data_no = random.randint(1,150)
+test_data = []
+for row in Irisreader:
+    if row[0] == str(test_data_no):
+        test_data = [row[1],row[2],row[3],row[4]] #this is specific to this database so if changing the code this must be edited.
+flower = Network(2,5,test_data,3)
+all_weights = flower.total_weights(test_data)
+
+print(all_weights)
+
+
 
 
 
--- test.py
+++ test.py
@@ -1,10 +1,26 @@
 import csv
-
-Iris = open("Iris.csv","r")
-Irisreader = csv.reader(Iris)
-for row in Irisreader:
-    if row[0] == "50":
-        print(row)
+import random
 
 
+def database_test():
+    Iris = open("Iris.csv", "r")
+    Irisreader = csv.reader(Iris)
+    for row in Irisreader:
+        if row[0] == "50":
+            print(row)
 
+
+def weight_set_test():
+    input_weights = []
+    weights = []
+    for y in range(0, 4):
+        for x in range(0, 4):
+            # z = round(random.uniform(-1,1),2)
+            print(input_weights)
+            input_weights.append(2)
+        weights[y] = input_weights
+        input_weights = []
+    return weights
+
+
+weight_set_test()
