8a6cc8f0ab1aba6a64098a39f22da2d34b525fd3
LaurieBeebee <lbeebee15@heathfieldcc.co.uk>
2022-02-08 11:43:39 +0000

    commit

--- Project_Vectorised1.py
+++ Project_Vectorised1.py
@@ -1,11 +1,11 @@
 import numpy as np
-import random
 import csv
 import time
 
 
 # 2 hidden layers, 5 neurons each
 
+LEARNING_RATE = 0.005
 
 X = np.zeros((4, 150), float)
 Y = np.zeros((3, 150), int)
@@ -67,41 +67,107 @@ def all_z_activations(w0, w1, w2, b1, b2, b3, x):
     a3 = sigmoid(z3)
     return z1, z2, z3, a1, a2, a3
 
-def errors(w0, w1, w2, z1, z2, z3, a3, Y):
+def errors(w1, w2, z1, z2, z3, a3, Y):
     errors3 = (a3 - Y) * inv_sigmoid(z3)
     errors2 = np.dot(np.transpose(w2), errors3)*inv_sigmoid(z2)
-    errors1 = np.dot(np.transpose(w1), errors2)
+    errors1 = np.dot(np.transpose(w1), errors2)*inv_sigmoid(z1)
     return errors1, errors2, errors3
 
+def deriv_cost_to_weight(errors1, errors2, errors3, a1, a2, x):
+    dw0 = 1/np.shape(x)[1]*(np.dot(errors1, np.transpose(x)))
+    dw1 = 1/np.shape(x)[1]*(np.dot(errors2, np.transpose(a1)))
+    dw2 = 1/np.shape(x)[1]*(np.dot(errors3, np.transpose(a2)))
+    return dw0, dw1, dw2
+
+def gradient_descent(w0, w1, w2, b1, b2, b3, error1, error2, error3, dw0, dw1, dw2, x):
+    w0 -= LEARNING_RATE*dw0
+    w1 -= LEARNING_RATE*dw1
+    w2 -= LEARNING_RATE*dw2
+    b1 -= LEARNING_RATE*(1/np.shape(x)[1])*np.sum(error1, axis=1, keepdims=True)
+    b2 -= LEARNING_RATE*(1/np.shape(x)[1])*np.sum(error2, axis=1, keepdims=True)
+    b3 -= LEARNING_RATE*(1/np.shape(x)[1])*np.sum(error3, axis=1, keepdims=True)
+    return w0, w1, w2, b1, b2, b3
+
+def cost(Y, a3):
+    cost = 1/np.shape(Y)[1]*np.sum((Y - a3)**2)
+    return cost
+
+def main():
+    tic = time.time()
+    weights = weight_set()
+    biases = bias_set()
+    z_and_activations = all_z_activations(weights[0], weights[1], weights[2], biases[0], biases[1], biases[2], X)
+    toc = time.time()
+    while cost(Y, z_and_activations[5]) > 0 and toc-tic <= 300:
+        z_and_activations = all_z_activations(weights[0], weights[1], weights[2], biases[0], biases[1], biases[2], X)
+        error = errors(weights[1], weights[2], z_and_activations[0], z_and_activations[1], z_and_activations[2], z_and_activations[5], Y)
+        derivitive_cost_to_weight = deriv_cost_to_weight(error[0], error[1], error[2], z_and_activations[3], z_and_activations[4], X)
+        change_weights_and_biases = gradient_descent(weights[0], weights[1], weights[2], biases[0], biases[1], biases[2], error[0], error[1], error[2], derivitive_cost_to_weight[0], derivitive_cost_to_weight[1], derivitive_cost_to_weight[2], X)
+        cost1= cost(Y, z_and_activations[5])
+        print(cost1)
+        toc = time.time()
+    print(test(weights[0], weights[1], weights[2], biases[0], biases[1], biases[2]))
+
+def test(w0, w1, w2, b1, b2, b3):
+    X = np.zeros((4, 150), float)
+    Y = np.zeros((3, 150), int)
+
+    correct = 0
+
+    Iris = open("Iris.csv", "r")
+    Irisreader = csv.reader(Iris)
+    count = 0
+    for row in Irisreader:
+        if row[0] != "Id":
+            # if (int(row[0]) >= 40 and int(row[0]) < 51) or (int(row[0]) >= 90 and int(row[0]) < 101) or (int(row[0]) >= 140 and int(row[0]) < 151):
+                X[0, count] = float(row[1])
+                X[1, count] = float(row[2])
+                X[2, count] = float(row[3])
+                X[3, count] = float(row[4])
+                if row[5] == "Iris-setosa":
+                    Y[0, count] = 1
+                    Y[1, count] = 0
+                    Y[2, count] = 0
+                elif row[5] == "Iris-versicolor":
+                    Y[0, count] = 0
+                    Y[1, count] = 1
+                    Y[2, count] = 0
+                elif row[5] == "Iris-virginica":
+                    Y[0, count] = 0
+                    Y[1, count] = 0
+                    Y[2, count] = 1
+                count += 1
+    Iris.close()
+    activations = all_z_activations(w0, w1, w2, b1, b2, b3, X)
+    for i in range(0, np.shape(X)[1]):
+        if activations[5][0,i] > activations[5][1,i] and activations[5][0,i] > activations[5][2,i]:
+            output = "Iris-setosa"
+            if Y[0,i] == 1:
+                correct += 1
+        elif activations[5][1,i] > activations[5][0,i] and activations[5][1,i] > activations[5][2,i]:
+            output = "Iris-versicolor"
+            if Y[1,i] == 1:
+                correct += 1
+        elif activations[5][2,i] > activations[5][1,i] and activations[5][2,i] > activations[5][0,i]:
+            output = "Iris-virginica"
+            if Y[2,i] == 1:
+                correct += 1
+    return (correct/count)*100
 
 
 
 
 
+main()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# weights = weight_set()
+# biases = bias_set()
+# z_and_activations = all_z_activations(weights[0], weights[1], weights[2], biases[0], biases[1], biases[2], X)
+# errors = errors(weights[1], weights[2], z_and_activations[0], z_and_activations[1], z_and_activations[2], z_and_activations[5], Y)
+# deriv_cost_to_weight = deriv_cost_to_weight(errors[0], errors[1], errors[2], z_and_activations[3], z_and_activations[4], X)
+# gradient_descent = gradient_descent(weights[0], weights[1], weights[2], biases[0], biases[1], biases[2], errors[0], errors[1], errors[2], deriv_cost_to_weight[0], deriv_cost_to_weight[1], deriv_cost_to_weight[2], X)
+# cost_1 = cost(Y, z_and_activations[5])
+# print(cost_1)
 
 #
 # class Network:
